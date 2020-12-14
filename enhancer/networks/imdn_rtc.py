a1490f19fb5b00fde3a0d866a8db8b6592fe7a20
vinotharjun <vinotharjun@hotmail.com>
2020-12-14 15:49:53 +0000

    minor fixes

--- enhancer/networks/imdn_rtc.py
+++ enhancer/networks/imdn_rtc.py
@@ -14,9 +14,9 @@ class SmallEnhancer(nn.Module):
                                       B.conv_layer(self.nf, self.nf, 3, stride=1,bias=False)]
         rb_blocks = [B.IMDModule_Large(in_channels=self.nf) for _ in range(self.num_modules)]
         LR_conv = B.conv_layer(self.nf, self.nf, 3,bias=False)
-        upsample_block = B.interpolation_block
+        upsample_block = B.pixelshuffle_block
         upsampler = upsample_block(self.nf,self.out_nc,self.upscale)
-#         upsampler = B.pixelshuffle_block(self.nf,self.out_nc,self.upscale)
+        upsampler = upsampler(self.nf,self.out_nc,self.upscale)
 #         upsampler = B.PixelShuffle_ICNR(self.nf,self.out_nc,scale=self.upscale,blur=False)
         self.model = B.sequential(*fea_conv, B.ShortcutBlock(B.sequential(*rb_blocks, LR_conv)),
                                   *upsampler)
