458daca6383cc2075d692c6f0b9c8420b3b56fb7
vinotharjun <vinotharjun@hotmail.com>
2020-12-28 16:11:51 +0000

    data changes

--- enhancer/data/lqgt.py
+++ enhancer/data/lqgt.py
@@ -105,12 +105,17 @@ class LQGT(data.Dataset):
         gt_h, gt_w = img_GT.size()[-1], img_GT.size()[-2]
 
         if gt_h != lr_h * self.scale or gt_w != lr_w * self.scale:
-            img_GT = torch.nn.functional.interpolate(
-                img_GT.unsqueeze(0), size=(self.GT_SIZE, self.GT_SIZE), mode="bicubic",align_corners=True
-            ).squeeze(0)
+#             img_GT = torch.nn.functional.interpolate(
+#                 img_GT.unsqueeze(0), size=(self.GT_SIZE, self.GT_SIZE), mode="bicubic",align_corners=True
+#             ).squeeze(0)
             img_LQ = torch.nn.functional.interpolate(
                 img_LQ.unsqueeze(0),
                 size=(self.GT_SIZE // self.scale, self.GT_SIZE // self.scale),
+                mode="bicubic",align_corners=True
+            ).squeeze(0)
+            img_GT = torch.nn.functional.interpolate(
+                img_GT.unsqueeze(0),
+                size=(int(self.GT_SIZE // self.scale) * self.scale, int(self.GT_SIZE // self.scale)*self.scale),
                 mode="bicubic",align_corners=True
             ).squeeze(0)
         if self.noise_rate_random ==True:
