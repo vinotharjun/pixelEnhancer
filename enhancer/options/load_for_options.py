8f11256f2fc7d2397e2da4997eaa3dd4acf18b49
Vinod Arjun <32776658+vinotharjun@users.noreply.github.com>
2021-01-27 22:34:58 +0530

    Update load_for_options.py

--- enhancer/options/load_for_options.py
+++ enhancer/options/load_for_options.py
@@ -20,7 +20,7 @@ def get_dataloader_from_yml(yml_file_path):
         ops["datasets"]["train"]["noise_needed"],
         ops["datasets"]["train"]["GT_size"],
         ops["scale"],
-        ops["datasets"]["train"]["noise_rate_random"]
+        ops["datasets"]["train"]["noise_rate_random"],
     )
     val_dataset = create_dataset(
         ops["datasets"]["val"]["mode"],
@@ -30,7 +30,7 @@ def get_dataloader_from_yml(yml_file_path):
         ops["datasets"]["val"]["noise_needed"],
         ops["datasets"]["val"]["GT_size"],
         ops["scale"],
-        ops["datasets"]["val"]["noise_rate_random"]
+        ops["datasets"]["val"]["noise_rate_random"],
     )
     train_loader = create_dataloader(
         train_dataset,
@@ -46,6 +46,18 @@ def get_dataloader_from_yml(yml_file_path):
         "train_dataloader": train_loader,
         "validation_dataloader": val_loader,
     }
+
+
+def load_partial(generator, path, strict=False, key=None):
+    model_dict = generator.state_dict()
+    if key is not None:
+        pretrained_dict = torch.load(path, map_location=device)[key]
+    else:
+        pretrained_dict = torch.load(path, map_location=device)
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    model_dict.update(pretrained_dict)
+    generator.load_state_dict(model_dict, strict=strict)
+    return generator
 
 
 def get_generator_from_yml(yml_file_path, pretrain_path=None, key=None, strict=True):
@@ -64,12 +76,21 @@ def get_generator_from_yml(yml_file_path, pretrain_path=None, key=None, strict=T
             gc = opt["structure"]["network_G"]["gc"]
         else:
             gc = 32
-        if scale in [2, 4, 8, 16,3,6]:
+        if scale in [2, 4, 8, 16, 3, 6]:
             pass
         else:
             scale = 4
-        net = importlib.import_module("enhancer.networks.{}".format(model_name)).SmallEnhancer
-        model = net(in_nc=in_c, nf=nf, num_modules=num_modules, out_nc=out_c, upscale=scale, gc=gc)
+        net = importlib.import_module(
+            "enhancer.networks.{}".format(model_name)
+        ).SmallEnhancer
+        model = net(
+            in_nc=in_c,
+            nf=nf,
+            num_modules=num_modules,
+            out_nc=out_c,
+            upscale=scale,
+            gc=gc,
+        )
         model = model.to(device)
     else:
         in_c = opt["structure"]["network_G"]["in_nc"]
@@ -78,65 +99,75 @@ def get_generator_from_yml(yml_file_path, pretrain_path=None, key=None, strict=T
         num_modules = opt["structure"]["network_G"]["num_modules"]
         scale = opt["scale"]
         model_name = opt["structure"]["network_G"]["which_model_G"]
-        if scale in [2, 4, 8, 16,3,6]:
+        if scale in [2, 4, 8, 16, 3, 6]:
             pass
         else:
             scale = 4
-        net = importlib.import_module("enhancer.networks.{}".format(model_name)).SmallEnhancer
-        model = net(in_nc=in_c, nf=nf, num_modules=num_modules, out_nc=out_c, upscale=scale)
+        net = importlib.import_module(
+            "enhancer.networks.{}".format(model_name)
+        ).SmallEnhancer
+        model = net(
+            in_nc=in_c, nf=nf, num_modules=num_modules, out_nc=out_c, upscale=scale
+        )
         model = model.to(device)
 
     if pretrain_path is False:
-        return model 
+        return model
     if pretrain_path is not None:
-        if key is not None:
-            model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device)[key], strict=strict)
-            print(f"generator is loaded from {pretrain_path}")
-        else:
-            model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device), strict=strict)
-            print(f"generator is loaded from {pretrain_path}")
+        model = load_partial(model.to(device), pretrain_path, strict, key)
+        print(f"generator is loaded from {pretrain_path}")
+        model.to(device)
     else:
         if opt["pretraining_settings"]["network_G"]["want_load"] is True:
-            pretrain_path = opt["pretraining_settings"]["network_G"]["pretrained_model_path"]
+            pretrain_path = opt["pretraining_settings"]["network_G"][
+                "pretrained_model_path"
+            ]
             strict = opt["pretraining_settings"]["network_G"]["pretrained_model_path"]
             key = opt["pretraining_settings"]["network_G"]["key"]
-            if key is not None:
-                model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device)[key], strict=strict)
-                print(f"generator is loaded from {pretrain_path}")
-            else:
-                model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device), strict=strict)
-                print(f"generator is loaded from {pretrain_path}")
-            
-                
+            model = load_partial(model.to(device), pretrain_path, strict, key)
+            print(f"generator is loaded from {pretrain_path}")
+            model.to(device)
     return model
 
 
-def get_discriminator_from_yml(yml_file_path, pretrain_path=None, key=None, strict=True):
+def get_discriminator_from_yml(
+    yml_file_path, pretrain_path=None, key=None, strict=True
+):
     if yml_file_path is None:
         raise Exception("need yml file")
     opt = parse_yml(yml_file_path)
     if "network_D" not in opt["pretraining_settings"]:
-        return 
+        return
     model = Discriminator().to(device)
     if pretrain_path is False:
-        return model 
+        return model
     if pretrain_path is not None:
         if key is not None:
-            model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device)[key], strict=strict)
+            model.to(device).load_state_dict(
+                torch.load(pretrain_path, map_location=device)[key], strict=strict
+            )
             print(f"critic is loaded from {pretrain_path}")
         else:
-            model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device), strict=strict)
+            model.to(device).load_state_dict(
+                torch.load(pretrain_path, map_location=device), strict=strict
+            )
             print(f"critic is loaded from {pretrain_path}")
     else:
         if opt["pretraining_settings"]["network_D"]["want_load"] is True:
-            pretrain_path = opt["pretraining_settings"]["network_D"]["pretrained_model_path"]
+            pretrain_path = opt["pretraining_settings"]["network_D"][
+                "pretrained_model_path"
+            ]
             strict = opt["pretraining_settings"]["network_D"]["pretrained_model_path"]
             key = opt["pretraining_settings"]["network_D"]["key"]
             if key is not None:
-                model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device)[key], strict=strict)
+                model.to(device).load_state_dict(
+                    torch.load(pretrain_path, map_location=device)[key], strict=strict
+                )
                 print(f"critic is loaded from {pretrain_path}")
             else:
-                model.to(device).load_state_dict(torch.load(pretrain_path,map_location=device), strict=strict)
+                model.to(device).load_state_dict(
+                    torch.load(pretrain_path, map_location=device), strict=strict
+                )
                 print(f"critic is loaded from {pretrain_path}")
     return model
 
@@ -178,15 +209,19 @@ def get_trainer_from_yml(
         save_checkpoint_file_name = opt["train_settings"]["save_checkpoint_file_name"]
         save_bestmodel_file_name = opt["train_settings"]["save_bestmodel_file_name"]
         if opt["pretraining_settings"]["network_G"]["want_load"] == True:
-            load_checkpoint_file_path_G = opt["pretraining_settings"]["network_G"]["pretrained_model_path"]
+            load_checkpoint_file_path_G = opt["pretraining_settings"]["network_G"][
+                "pretrained_model_path"
+            ]
         else:
             load_checkpoint_file_path_G = None
         if opt["pretraining_settings"]["network_D"]["want_load"] == True:
-            load_checkpoint_file_path_D = opt["pretraining_settings"]["network_D"]["pretrained_model_path"]
+            load_checkpoint_file_path_D = opt["pretraining_settings"]["network_D"][
+                "pretrained_model_path"
+            ]
         else:
             load_checkpoint_file_path_D = None
-            
-#         load_checkpoint_file_path = opt["train_settings"]["load_checkpoint_file_path"]
+
+        #         load_checkpoint_file_path = opt["train_settings"]["load_checkpoint_file_path"]
         sample_interval = opt["train_settings"]["sample_interval"]
         trainer = GANTrainer(
             generator=model_G,
@@ -203,10 +238,10 @@ def get_trainer_from_yml(
             save_checkpoint_folder_path=save_checkpoint_folder_path,
             save_checkpoint_file_name=save_checkpoint_file_name,
             save_best_file_name=save_bestmodel_file_name,
-            load_checkpoint_file_path_generator =load_checkpoint_file_path_G,
-            load_checkpoint_file_path_critic = load_checkpoint_file_path_D,
+            load_checkpoint_file_path_generator=load_checkpoint_file_path_G,
+            load_checkpoint_file_path_critic=load_checkpoint_file_path_D,
             sample_interval=sample_interval,
-            feature_criterion = criterion
+            feature_criterion=criterion,
         )
         return trainer
     else:
@@ -232,7 +267,9 @@ def get_trainer_from_yml(
         save_checkpoint_file_name = opt["train_settings"]["save_checkpoint_file_name"]
         save_bestmodel_file_name = opt["train_settings"]["save_bestmodel_file_name"]
         if opt["pretraining_settings"]["network_G"]["want_load"] == True:
-            load_checkpoint_file_path = opt["pretraining_settings"]["network_G"]["pretrained_model_path"]
+            load_checkpoint_file_path = opt["pretraining_settings"]["network_G"][
+                "pretrained_model_path"
+            ]
         else:
             load_checkpoint_file_path = None
         sample_interval = opt["train_settings"]["sample_interval"]
@@ -253,7 +290,7 @@ def get_trainer_from_yml(
             criterion=criterion,
             load_checkpoint_file_path=load_checkpoint_file_path,
             sample_interval=sample_interval,
-            lr_step_decay = lr_step_decay
+            lr_step_decay=lr_step_decay,
         )
         return trainer
 
@@ -280,4 +317,9 @@ def load_pipeline_from_yml(yml_file_path):
         model_D=discriminator,
     )
 
-    return {"loaders":loaders, "generator":generator, "critic":discriminator, "trainer":trainer}
+    return {
+        "loaders": loaders,
+        "generator": generator,
+        "critic": discriminator,
+        "trainer": trainer,
+    }
